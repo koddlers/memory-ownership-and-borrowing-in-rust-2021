0d32d77321ebf763fca6eb0ac80b0c3e20e23369
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-06-15 23:00:26 +0600

    Add Module 03 - Managing Memory with Ownership - Lesson 04 - Non-copyable Data Types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+#[allow(unused)]
+
 mod module_03;
 
 use module_03::managing_memory_with_ownership;
 
 fn main() {
     // Module 03 - Managing Memory with Ownership
-    managing_memory_with_ownership::copyable_data_types();
+    // managing_memory_with_ownership::copyable_data_types();
+    managing_memory_with_ownership::non_copyable_data_types();
 }
--- src/module_03.rs
+++ src/module_03.rs
@@ -10,9 +10,39 @@ pub mod managing_memory_with_ownership {
         // moving the value pointed to by `a` to `b`
         let b = a;
 
-        // this line will produce the error: `value borrowed here after move`
+        // the following line will produce the error: `value borrowed here after move`
         // println!("a: {}", a);
         // while this line is fine
         println!("b: {}", b);
+    }
+
+    fn print_string(a: &String) {
+        println!("This is the value of my string: {:?}", a);
+    }
+
+    pub fn non_copyable_data_types() {
+        let x = String::from("42");
+        let y = x;
+
+        // the following line will produce the error: `value borrowed here after move`
+        // println!("x: {}", x);
+        println!("y: {}", y);
+
+        let x = String::from("42");
+
+        // print_string(x);
+        // the following line will produce the error: `value used here after move`
+        // print_string(x);
+
+        // there are two ways to solve this
+        // solution 1: to send a copy of the data using the `clone()`
+        // which is a deep copy and expensive
+        // print_string(x.clone());
+        // print_string(x);
+
+        // alternatively, the better way to do this is by sending an immutable reference
+        // thus the function `print_string` borrows the value of the variable being sent
+        print_string(&x);
+        print_string(&x);
     }
 }
