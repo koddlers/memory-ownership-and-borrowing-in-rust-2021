a1c8e2123d522e841384ae1624e751d719e57af1
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-06-16 22:55:38 +0600

    Add Module 05 - Using Lifetimes to Reduce Ambiguity - Lesson (01, 04)

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod module_05;
 use module_03::managing_memory_with_ownership;
 use module_04::borrowing_values_by_reference;
 use module_05::using_lifetimes_to_reduce_ambiguity;
+use module_05::using_lifetimes_to_reduce_ambiguity_v2;
 
 fn main() {
     // Module 03 - Managing Memory with Ownership
@@ -20,5 +21,6 @@ fn main() {
     // borrowing_values_by_reference::string_slices();
 
     // Module 05 - Using Lifetimes to Reduce Ambiguity
-    using_lifetimes_to_reduce_ambiguity::lifetimes_in_functions();
+    // using_lifetimes_to_reduce_ambiguity::lifetimes_in_functions();
+    using_lifetimes_to_reduce_ambiguity_v2::lifetimes_in_structs();
 }
--- src/module_05.rs
+++ src/module_05.rs
@@ -24,3 +24,35 @@ pub mod using_lifetimes_to_reduce_ambiguity {
         println!("The oldest person is: {:?}", person_3)
     }
 }
+
+pub mod using_lifetimes_to_reduce_ambiguity_v2 {
+    #[derive(Debug)]
+    struct Person<'a> {
+        name: String,
+        age: u8,
+        favorite_fruit: &'a str,
+    }
+
+    pub fn lifetimes_in_structs() {
+        let winter_fruits = [
+            "Apples".to_string(),
+            "Clementines".to_string(),
+            "Grapefruit".to_string(),
+            "Kiwis".to_string()
+        ];
+
+        let person_1 = Person {
+            name: "John".to_string(),
+            age: 20,
+            favorite_fruit: &winter_fruits[0],
+        };
+        println!("person_1: {:?}", person_1);
+
+        let person_2 = Person {
+            name: "Jane".to_string(),
+            age: 30,
+            favorite_fruit: &winter_fruits[1],
+        };
+        println!("person_2: {:?}", person_2);
+    }
+}
